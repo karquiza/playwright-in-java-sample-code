1c7e213cf9ceee6a519db811c6397c9fc1840882
Karen Junsay <>
2026-02-21 17:45:11 -0500

    progress after section 2 - my first playwright tests with test fixtures refactoring

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,13 +3,30 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class ASimplePlaywrightTest {
 
     @Test
-    void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+    void shouldShowThePageTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void shouldSearchByKeyword(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+
+        page.locator("[placeholder=Search]").fill("pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+        Assertions.assertTrue(matchingSearchResults > 0);
     }
 }
